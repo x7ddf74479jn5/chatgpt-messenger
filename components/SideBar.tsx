fced3ccacbce2ddd218c823850b7636f512e690b
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-03-02 23:03:29 +0900

    ✨ feat: create new chat

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,11 +1,19 @@
 "use client";
 
+import { collection, orderBy, query } from "firebase/firestore";
 import { useSession, signOut } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { db } from "../firebase";
+import { ChatRow } from "./ChatRow";
 
 import { NewChat } from "./NewChat";
 
 export const SideBar = () => {
   const { data: session } = useSession();
+
+  const [chats, loading, error] = useCollection(
+    session && query(collection(db, "users", session?.user?.email!, "chats"), orderBy("createdAt", "asc"))
+  );
 
   return (
     <div className="flex flex-col p-2 h-screen">
@@ -14,6 +22,10 @@ export const SideBar = () => {
           <NewChat />
 
           <div></div>
+
+          {chats?.docs.map((chat) => (
+            <ChatRow key={chat.id} id={chat.id} />
+          ))}
         </div>
       </div>
 
