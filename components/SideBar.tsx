7413729677f081293a3103cbdae31ec23492074f
x7ddf74479jn5 <x7ddf74479jn5@gmail.com>
2023-03-02 22:07:25 +0900

    ✨ feat: impl avatar and sign-out in SideBar

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import { useSession, signOut } from "next-auth/react";
+
 import { NewChat } from "./NewChat";
 
 export const SideBar = () => {
+  const { data: session } = useSession();
+
   return (
     <div className="flex flex-col p-2 h-screen">
       <div className="flex-1">
@@ -10,6 +16,16 @@ export const SideBar = () => {
           <div></div>
         </div>
       </div>
+
+      {session && (
+        <button onClick={() => signOut()}>
+          <img
+            src={session?.user?.image!}
+            alt="Profile pic"
+            className="h-12 w-12 rounded-full cursor-pointer mx-auto mb-2 hover:opacity-50"
+          />
+        </button>
+      )}
     </div>
   );
 };
